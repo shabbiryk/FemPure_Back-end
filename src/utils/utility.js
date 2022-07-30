0034ae1df53ffe14b267aec8e7250e95d2e6bfeb
Bash <contact.qmicon@gmail.com>
2022-07-30 16:49:31 +0530

    added smart contract Interface functions

--- src/utils/utility.js
+++ src/utils/utility.js
@@ -3,13 +3,13 @@ import { requestAccount, getSignedContract } from "./common";
 
 // SmartContract function calls
 
-export async function registerUser(contractAddr, artifact) {
+export async function registerUser(contractAddr, artifact, pincode, residenceAddress) {
     if (typeof window.ethereum != undefined) {
       await requestAccount();
   
       const fempureContract = getSignedContract(contractAddr, artifact);
       try {
-        let transaction = await fempureContract.registerUser(400097,"some address");
+        let transaction = await fempureContract.registerUser(pincode, residenceAddress);
         // let receipt = await transaction.wait();
         console.log(transaction);
       } catch (err) {
@@ -25,11 +25,91 @@ export async function registerUser(contractAddr, artifact) {
   
       const fempureContract = getSignedContract(contractAddr, artifact);
       try {
-        let transaction = await fempureContract.isRegisteredUser();
+        let response = await fempureContract.isRegisteredUser();
+        // let receipt = await transaction.wait();
+        console.log(response);
+        return response;
+      } catch (err) {
+        console.log("error", err)
+    }
+    }
+  }
+
+  export async function registerPartner(contractAddr, artifact, pincodes) {
+    if (typeof window.ethereum != undefined) {
+      await requestAccount();
+  
+      const fempureContract = getSignedContract(contractAddr, artifact);
+      try {
+        let transaction = await fempureContract.registerPartner(pincodes);
         // let receipt = await transaction.wait();
         console.log(transaction);
       } catch (err) {
+        console.log("error")
+    }
+    }
+  }
+
+  export async function isRegisteredPartner(contractAddr, artifact) {
+    if (typeof window.ethereum != undefined) {
+      await requestAccount();
+  
+      const fempureContract = getSignedContract(contractAddr, artifact);
+      try {
+        let response = await fempureContract.isRegisteredPartner();
+        // let receipt = await transaction.wait();
+        console.log(response);
+        return response;
+      } catch (err) {
         console.log("error", err)
+    }
+    }
+  }
+
+  export async function productMetadataURI(contractAddr, artifact, productID) {
+    if (typeof window.ethereum != undefined) {
+      await requestAccount();
+  
+      const fempureContract = getSignedContract(contractAddr, artifact);
+      try {
+        let ipfs_uri = await fempureContract.productMetadataURI(productID);
+        // let receipt = await transaction.wait();
+        console.log(ipfs_uri);
+        return ipfs_uri;
+      } catch (err) {
+        console.log("error")
+    }
+    }
+  }
+
+  export async function getAvailableProductsInLocation(contractAddr, artifact, pincode) {
+    if (typeof window.ethereum != undefined) {
+      await requestAccount();
+  
+      const fempureContract = getSignedContract(contractAddr, artifact);
+      try {
+        let productIDs = await fempureContract.getAvailableProductsInLocation(pincode);
+        // let receipt = await transaction.wait();
+        console.log(productIDs);
+        return productIDs;
+      } catch (err) {
+        console.log("error")
+    }
+    }
+  }
+
+  export async function getAvailableLocations(contractAddr, artifact) {
+    if (typeof window.ethereum != undefined) {
+      await requestAccount();
+  
+      const fempureContract = getSignedContract(contractAddr, artifact);
+      try {
+        let pincodes = await fempureContract.getAvailableLocations();
+        // let receipt = await transaction.wait();
+        console.log(pincodes);
+        return pincodes;
+      } catch (err) {
+        console.log("error")
     }
     }
   }
